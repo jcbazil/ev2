ab82ab8c622985130d0e7370cd11ba3ed06be536
jcbazil <jcbazil@gmail.com>
2017-06-13 09:35:21 +0800

    sch/sch.php
    for calender display

---
+++ sch.php
@@ -0,0 +1,366 @@
+<?php
+require_once('../config.php');
+$sql_client = "SELECT * FROM cit_events ";
+$result = $conn->query($sql_client);
+
+while( $row =$result->fetch_array() ) {
+
+            $events[] = $row; 
+        }
+         json_encode($events); 
+
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>eMonster 2.0</title>
+  <?php //include ('../csslink.php'); ?>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="autocomplete/jquery.autocomplete.css" />
+<script type="text/javascript" src="autocomplete/jquery.js"></script>
+<script type="text/javascript" src="autocomplete/jquery.autocomplete.js"></script>
+
+    <title>Scheduling</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- FullCalendar -->
+	<link href='css/fullcalendar.css' rel='stylesheet' />
+	
+<script>
+$(document).ready(function(){
+ $("#tag").autocomplete("que.php", {
+		selectFirst: true
+	});
+
+$('#results').html(data);
+
+}
+);
+</script>
+
+    <!-- Custom CSS -->
+    <style>
+		body {
+			padding-top: 70px;
+			/* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+		}
+		#calendar {
+			max-width: 1200px;
+		}
+		.col-centered{
+			float: none;
+			margin: 0 auto;
+		}
+		#wgtmsr option{
+		  width:150px;   
+		}
+    </style>
+
+</head>
+<body>
+<div class="wrapper">
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Back to main</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Back To Main</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Menu</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+	<div class="container">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">                
+                <div id="calendar" class="col-centered">
+                </div>
+            </div>			
+        </div>
+        <!-- /.row -->
+		
+		<!-- Modal -->
+		<div class="modal fade" id="ModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<form class="form-horizontal" method="POST" action="addEvent.php"> <!-- Add Scheduling-->
+			
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Schedule</h4>
+			  </div>
+			  <div class="modal-body">
+				
+				  <div class="form-group">
+				   <label>Project Auto:</label>
+					<input name="tag" type="text" id="tag" size="20"/>
+					<label for="title" class="col-sm-2 control-label">Project</label>
+					<div class="col-sm-10">
+						<select name='pid' style="width:150px">
+						<?php
+							
+							echo "<option value='$st_id'>$st</option>";
+							
+							$que = "SELECT p_title,client,p_id,p_gov FROM vw_project "; 
+							$re2 = $conn->query($que);
+							while($rowz = $re2 ->fetch_array()){
+							
+							
+							$ptitle=$rowz['p_title'];
+							$pid=$rowz['p_id'];
+							$client=$rowz['client'];
+							
+								echo "<option value='$pid'>$ptitle- $client</option>"."<BR>";
+								echo "<br />";
+							}echo "</select>"; 
+							
+						?>		
+						
+					</div>
+				  </div>  
+				  <div class="form-group">
+					<label for="title" class="col-sm-2 control-label">Title</label>
+					<div class="col-sm-10">
+					  <input type="text" name="title" class="form-control" id="title" placeholder="Title">
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="color" class="col-sm-2 control-label">Color</label>
+					<div class="col-sm-10">
+					  <select name="color" class="form-control" id="color">
+						  <option value="">Choose</option>
+						  <option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
+						  <option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
+						  <option style="color:#008000;" value="#008000">&#9724; Green</option>						  
+						  <option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
+						  <option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
+						  <option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
+						  <option style="color:#000;" value="#000">&#9724; Black</option>
+						  
+						</select>
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="start" class="col-sm-2 control-label">Start date</label>
+					<div class="col-sm-10">
+					  <input type="text" name="start" class="form-control" id="start" >
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="end" class="col-sm-2 control-label">End date</label>
+					<div class="col-sm-10">
+					  <input type="text" name="end" class="form-control" id="end" >
+					</div>
+				  </div>
+				
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="submit" class="btn btn-primary">Save changes</button>
+			  </div>
+			</form>
+			</div>
+		  </div>
+		</div>
+		
+		
+		
+		<!-- Modal -->
+		<div class="modal fade" id="ModalEdit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<form class="form-horizontal" method="POST" action="editEventTitle.php">
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Edit Event</h4>
+			  </div>
+			  <div class="modal-body">
+				
+				  <div class="form-group">
+					<label for="title" class="col-sm-2 control-label">Title</label>
+					<div class="col-sm-10">
+					  <input type="text" name="title" class="form-control" id="title" placeholder="Title">
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="color" class="col-sm-2 control-label">Color</label>
+					<div class="col-sm-10">
+					  <select name="color" class="form-control" id="color">
+						  <option value="">Choose</option>
+						  <option style="color:#0071c5;" value="#0071c5">&#9724; Dark blue</option>
+						  <option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquoise</option>
+						  <option style="color:#008000;" value="#008000">&#9724; Green</option>						  
+						  <option style="color:#FFD700;" value="#FFD700">&#9724; Yellow</option>
+						  <option style="color:#FF8C00;" value="#FF8C00">&#9724; Orange</option>
+						  <option style="color:#FF0000;" value="#FF0000">&#9724; Red</option>
+						  <option style="color:#000;" value="#000">&#9724; Black</option>
+						  
+						</select>
+					</div>
+				  </div>
+				    <div class="form-group"> 
+						<div class="col-sm-offset-2 col-sm-10">
+						  <div class="checkbox">
+							<label class="text-danger"><input type="checkbox"  name="delete"> Delete event</label>
+						  </div>
+						</div>
+					</div>
+				  
+				  <input type="hidden" name="id" class="form-control" id="id">
+				
+				
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="submit" class="btn btn-primary">Save changes</button>
+			  </div>
+			</form>
+			</div>
+		  </div>
+		</div>
+
+    </div>
+  
+
+</div>
+<!-- ./wrapper -->
+
+ <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	<!-- FullCalendar -->
+	<script src='js/moment.min.js'></script>
+	<script src='js/fullcalendar.min.js'></script>
+	
+	<script>
+
+	$(document).ready(function() {
+		
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,basicWeek,basicDay'
+			},
+			defaultView: 'month',
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				
+				$('#ModalAdd #start').val(moment(start).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalAdd #end').val(moment(end).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalAdd').modal('show');
+			},
+			eventRender: function(event, element) {
+				element.bind('dblclick', function() {
+					$('#ModalEdit #id').val(event.id);
+					$('#ModalEdit #title').val(event.title);
+					$('#ModalEdit #color').val(event.color);
+					$('#ModalEdit').modal('show');
+				});
+			},
+			eventDrop: function(event, delta, revertFunc) { // si changement de position
+
+				edit(event);
+
+			},
+			eventResize: function(event,dayDelta,minuteDelta,revertFunc) { // si changement de longueur
+
+				edit(event);
+
+			},
+			events: [
+			<?php foreach($events as $event): 
+			
+				$start = explode(" ", $event['start']);
+				$end = explode(" ", $event['end']);
+				if($start[1] == '00:00:00'){
+					$start = $start[0];
+				}else{
+					$start = $event['start'];
+				}
+				if($end[1] == '00:00:00'){
+					$end = $end[0];
+				}else{
+					$end = $event['end'];
+				}
+			?>
+				{
+					id: '<?php echo $event['id']; ?>',
+					title: '<?php echo $event['title']; ?>',
+					start: '<?php echo $start; ?>',
+					end: '<?php echo $end; ?>',
+					color: '<?php echo $event['color']; ?>',
+				},
+			<?php endforeach; ?>
+			]
+		});
+		
+		function edit(event){
+			start = event.start.format('YYYY-MM-DD HH:mm:ss');
+			if(event.end){
+				end = event.end.format('YYYY-MM-DD HH:mm:ss');
+			}else{
+				end = start;
+			}
+			
+			id =  event.id;
+			
+			Event = [];
+			Event[0] = id;
+			Event[1] = start;
+			Event[2] = end;
+			
+			$.ajax({
+			 url: 'editEventDate.php',
+			 type: "POST",
+			 data: {Event:Event},
+			 success: function(rep) {
+					if(rep == 'OK'){
+						alert('Saved');
+					}else{
+						alert('Could not be saved. try again.'); 
+					}
+				}
+			});
+		}
+		
+	});
+
+</script>
+<?php include ('../jslink.php');?>
+</body>
+</html>
+
+
+
+
+
